fc3a28cbf9f2bc4af549ad9a8332b582399a0ab3
mayan-12 <mayansachan12@gmail.com>
2020-07-12 13:11:44 +0530

    portfolio started

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 //     });
 //   });
 
+
+
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -29,3 +31,23 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+
+
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// var navbar = document.getElementById("collapsable-nav");
+
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
